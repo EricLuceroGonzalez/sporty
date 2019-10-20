ef40cbfc9fc6d26af28f50002a5ad7145dc2518c
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2019-10-20 18:16:01 -0500

    getEquipos = () => api.get

--- src/api/index.js
+++ src/api/index.js
@@ -9,6 +9,7 @@ export const getSports = () => api.get("/deportes");
 export const getLigas = () => api.get("/liga");
 export const postLigas = content => api.post("/liga", content);
 export const postEquipo = content => api.post("/equipo", content);
+export const getEquipos = () => api.get("/equipos");
 export const postPlayer = aPlayer => api.post("/player", aPlayer);
 export const getLigaId = theId => api.get(`/ligaDetail/${theId}`);
 
@@ -16,6 +17,7 @@ const apis = {
   getLigas,
   getSports,
   getLigaId,
+  getEquipos,
   postLigas,
   postEquipo,
   postPlayer
